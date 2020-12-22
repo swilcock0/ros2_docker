25559319b962475a27f2c8fdbfe7dfdb5a78f0bd
Sam W <48917295+swilcock0@users.noreply.github.com>
2020-12-22 22:30:01 +0000

    RQT, Moveit
    Added RQT to the base and got moveit working. Just need to remember how I got the web bridge working previously now

--- Build.bat
+++ Build.bat
@@ -1,2 +1,3 @@
 docker build -t swilcock0/ros2_ubuntu:base .\base\.
 docker build -t swilcock0/ros2_ubuntu:moveit .\moveit_update\.
+docker build -t swilcock0/ros2_ubuntu:moveit_bridge .\moveit_bridge\.
