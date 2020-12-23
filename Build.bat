374a66aa7d0b47a2f25281a7fefcd055d87caab2
Sam W <48917295+swilcock0@users.noreply.github.com>
2020-12-23 01:59:22 +0000

    Fix upgradepermissions

--- Build.bat
+++ Build.bat
@@ -1,3 +1,20 @@
+@echo off
 docker build -t swilcock0/ros2_ubuntu:base .\base\.
+
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 docker build -t swilcock0/ros2_ubuntu:moveit .\moveit_update\.
+
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 docker build -t swilcock0/ros2_ubuntu:moveit_bridge .\moveit_bridge\.
+
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+docker image tag swilcock0/ros2_ubuntu:moveit_bridge swilcock0/ros2_ubuntu:latest
+
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+docker push swilcock0/ros2_ubuntu:latest
+
+echo Done!
