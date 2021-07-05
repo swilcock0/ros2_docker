55ab90749c080fcbb5932292a8c41f8712ecb79f
Sam W <48917295+swilcock0@users.noreply.github.com>
2021-07-05 15:48:26 +0100

    TigerVNC update

--- base/src/ubuntu/install/tigervnc.sh
+++ base/src/ubuntu/install/tigervnc.sh
@@ -2,4 +2,5 @@
 set -e
 
 echo "Install TigerVNC server"
-wget -qO- https://dl.bintray.com/tigervnc/stable/tigervnc-1.8.0.x86_64.tar.gz | tar xz --strip 1 -C /
+wget -qO- https://github.com/TigerVNC/tigervnc/archive/refs/tags/v1.11.0.tar.gz | tar xz --strip 1 -C /
+
