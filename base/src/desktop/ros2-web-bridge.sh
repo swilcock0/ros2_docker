4a485e79d66e8ff7466f47e37192c4077e3655e3
Sam W <48917295+swilcock0@users.noreply.github.com>
2021-09-06 17:19:02 +0100

    Updated step

--- base/src/desktop/ros2-web-bridge.sh
+++ base/src/desktop/ros2-web-bridge.sh
@@ -2,7 +2,7 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-source /opt/ros/foxy/setup.bash
+source /opt/ros/galactic/setup.bash
 source /home/ros/ros2_ws/install/setup.bash
 source /home/ros/moveit_ws/install/setup.bash
 source /home/ros/ros2api/install/setup.bash
