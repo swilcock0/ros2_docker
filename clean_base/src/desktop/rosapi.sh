9d56783de90087efab94c089d06af7f83a311eb2
Sam W <48917295+swilcock0@users.noreply.github.com>
2021-09-07 20:11:13 +0100

    Remove debug

--- clean_base/src/desktop/rosapi.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source ~/ros2api/install/setup.bash
-ros2 run ros2api ros2api
--- clean_base/src/ubuntu/install/tigervnc.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "Install TigerVNC server"
-wget -qO- https://github.com/TigerVNC/tigervnc/archive/refs/tags/v1.11.0.tar.gz | tar xz --strip 1 -C /
-
